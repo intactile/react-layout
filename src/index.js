8732d46fadae93841f5b5939fd3e7d1d2857676f
Thomas Paillot <paillot.t@intactile.com>
2021-12-24 15:03:46 +0100

    Export the 5 new layouts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 export { default as Box } from './Box';
 export { default as Center } from './Center';
 export { default as Cluster } from './Cluster';
+export { default as Cover } from './Cover';
+export { default as Frame } from './Frame';
+export { default as Grid } from './Grid';
+export { default as Imposter } from './Imposter';
+export { default as Reel } from './Reel';
 export { default as Sidebar } from './Sidebar';
 export { default as Stack } from './Stack';
 export { default as Switcher } from './Switcher';
